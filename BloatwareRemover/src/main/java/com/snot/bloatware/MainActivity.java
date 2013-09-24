0420e20cf17949b425d66a4ca9d2b5978e992f6d
Super Snot <sebastianthegreatful@gmail.com>
2013-09-24 02:46:31 +0200

    Added root check and warning dialog.

--- BloatwareRemover/src/main/java/com/snot/bloatware/MainActivity.java
+++ BloatwareRemover/src/main/java/com/snot/bloatware/MainActivity.java
@@ -1,7 +1,9 @@
 package com.snot.bloatware;
 
-import android.os.Bundle;
+import android.content.DialogInterface;
+import android.app.AlertDialog;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.widget.Toast;
 
@@ -13,16 +15,21 @@ public class MainActivity extends TabsFragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		String msg = "";
-		if (RootTools.isRootAvailable())
+		// TODO: don't show each time the phone is rotated... how?
+		if(!RootTools.isRootAvailable())
 		{
-			msg = "Root!";
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle(getString(R.string.dialog_noroot_title))
+			.setMessage(getString(R.string.dialog_noroot_message))
+			.setCancelable(false)
+			.setPositiveButton(getString(R.string.dialog_noroot_ok), new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int id) {
+					//do things
+					}
+				}
+			).show();
 		}
-		else
-		{
-			msg = "No root!";
-		}
-		Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
 
 		this.addTab("bloat", getString(R.string.tab_bloatware_title), BloatApplicationsFragment.class);
 		this.addTab("sys", getString(R.string.tab_system_apps_title), SystemApplicationsFragment.class);
