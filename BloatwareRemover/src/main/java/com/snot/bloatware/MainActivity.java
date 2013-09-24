71f7d3227ffedb450b269b56c733aab5ca849e28
Super Snot <sebastianthegreatful@gmail.com>
2013-09-24 12:15:29 +0200

    Added 'Dont show again' checkbox to root check dialog.

--- BloatwareRemover/src/main/java/com/snot/bloatware/MainActivity.java
+++ BloatwareRemover/src/main/java/com/snot/bloatware/MainActivity.java
@@ -1,11 +1,18 @@
 package com.snot.bloatware;
 
 import android.content.DialogInterface;
+import android.content.Context;
 import android.app.AlertDialog;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.Toast;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.CheckBox;
+import android.content.SharedPreferences;
+
 
 import com.stericson.RootTools.RootTools;;
 
@@ -15,21 +22,7 @@ public class MainActivity extends TabsFragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		// TODO: don't show each time the phone is rotated... how?
-		if(!RootTools.isRootAvailable())
-		{
-			AlertDialog.Builder builder = new AlertDialog.Builder(this);
-			builder.setTitle(getString(R.string.dialog_noroot_title))
-			.setMessage(getString(R.string.dialog_noroot_message))
-			.setCancelable(false)
-			.setPositiveButton(getString(R.string.dialog_noroot_ok), new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int id) {
-					//do things
-					}
-				}
-			).show();
-		}
+		checkRoot();
 
 		this.addTab("bloat", getString(R.string.tab_bloatware_title), BloatApplicationsFragment.class);
 		this.addTab("sys", getString(R.string.tab_system_apps_title), SystemApplicationsFragment.class);
@@ -43,6 +36,36 @@ public class MainActivity extends TabsFragmentActivity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void checkRoot()
+	{
+		final SharedPreferences settings = getSharedPreferences(MainActivity.class.getSimpleName(), Context.MODE_PRIVATE);
+		final String ROOT_DIALOG_SHOW = "dont_show";
+		if(!RootTools.isRootAvailable() && settings.getBoolean(ROOT_DIALOG_SHOW, true))
+		{
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			LayoutInflater inflater = LayoutInflater.from(this);
+			View view = inflater.inflate(R.layout.dialog_root_check_checkbox, null);
+			builder.setView(view);
+			final CheckBox dontShowAgain = (CheckBox)view.findViewById(R.id.dont_show_again);
+
+			builder.setTitle(getString(R.string.dialog_noroot_title))
+			.setMessage(getString(R.string.dialog_noroot_message))
+			.setCancelable(false)
+			.setPositiveButton(getString(R.string.dialog_noroot_ok), new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int id) {
+						if(dontShowAgain.isChecked())
+						{
+							SharedPreferences.Editor editor = settings.edit();
+							editor.putBoolean(ROOT_DIALOG_SHOW, false);
+							editor.commit();
+						}
+					}
+				}
+			).show();
+		}
 	}
 }
 
